2d46ddb661d12c3be9c32733b50cf4adbc9feb50
Mateus Valente <mateus.valente@outlook.com.br>
2025-09-30 21:31:50 -0300

    Configurando ansible

--- main.tf
+++ main.tf
@@ -28,7 +28,12 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name = "iac-alura"
-
+  # user_data = <<-EOF
+  #                #! /bin/bash
+  #                cd /home/ubuntu
+  #                echo "<h1>Feito com terraform</>" > index.html
+  #                nohup busybox httpd -f -p 8080 &
+  #                EOF
   tags = {
     Name = "learn-terraform"
   }
