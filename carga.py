b551f78bef93724eddd01ed1732173dbfab5edcf
Mateus Valente <mateus.valente@outlook.com.br>
2025-10-06 14:36:51 -0300

    Salvando arquivos

--- carga.py
+++
@@ -1,7 +0,0 @@
-from locust import FastHttpUser, task
-
-class WebsiteUser(FastHttpUser):
-
-    @task
-    def index(self):
-        self.client.get("/")
--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -1,11 +1,9 @@
-module "aws-prod" {
-  source = "../../infra"
-  instancia = "t2.micro"
-  regiao_aws = "us-east-1"
-  chave = "IaC-PROD"
-  grupoDeSeguranca = "prod"
-  minimo = 1
-  maximo = 10
-  nomeGrupo = "prod"
-  producao = true
+module "Producao" {
+  source = "../../Infra"
+
+  nome = "producao"
+  descricao = "aplicacao-de-producao"
+  max = 5
+  maquina = "t2.micro"
+  ambiente = "ambiente-de-producao"
 }
